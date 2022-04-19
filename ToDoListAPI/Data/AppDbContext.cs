722790eeabe393cf13a1209f10569f9422abf936
augustotakeo <augustotakeo@gmail.com>
2022-04-18 23:50:20 -0300

    feat: Configuration to map the model to the database

---
+++ ToDoListAPI/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Model;
+
+namespace ToDoListAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Annotation> Annotations { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("DataSource=app.db;Cache=Shared");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(entity =>
+            {
+                entity.HasKey(user => user.Id);
+            });
+
+            builder.Entity<Annotation>(entity =>
+            {
+                entity.HasKey(annotation => annotation.Id);
+                entity.HasOne(annotation => annotation.User)
+                      .WithMany(user => user.Annotations)
+                      .HasForeignKey(annotation => annotation.UserId);
+            });
+        }
+    }
+}
