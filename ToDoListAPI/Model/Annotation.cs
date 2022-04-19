271832d774462b5620a9f4de5bb3495e48284f48
augustotakeo <augustotakeo@gmail.com>
2022-04-18 23:47:57 -0300

    feat: application model

---
+++ ToDoListAPI/Model/Annotation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Model
+{
+    public class Annotation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime TaskScheduledFor { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
---
+++ ToDoListAPI/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool Checked { get; set; }
+
+        public virtual List<Annotation>? Annotations { get; set; }
+    }
+}
